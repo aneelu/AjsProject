5158cf12296d3bd0e4dc73e1c2fa4352bca78a79
Samba <samba.pendela@gmail.com>
2017-04-08 16:58:25 +0530

    Predefined Directives

---
+++ CodingStandardsApp/controllers/ctrl_one.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+    app.controller("ctrl_one",ctrl_one);
+    ctrl_one.$inject=["$scope"];
+    function ctrl_one($scope) {
+        $scope.var_one = "I am from Controller One";
+    }
+})();
+
+
+
