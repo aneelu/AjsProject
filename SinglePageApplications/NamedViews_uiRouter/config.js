82318aa82d4b94aad4a650784a9e0a3500dcff18
Samba <samba.pendela@gmail.com>
2017-04-09 13:14:43 +0530

    Single page Applications

--- SinglePageApplications/NamedViews_uiRouter/config.js
+++ SinglePageApplications/NamedViews_uiRouter/config.js
@@ -8,18 +8,15 @@ function config($stateProvider,$urlRouterProvider) {
                 templateUrl:"templates/page_one.html",
                 controller:"page_one"
             },
-
             'calc@page_one':{
                 templateUrl:"templates/calc.html",
                 controller:"calc"
             },
-
             'summary@page_one':{
                 templateUrl:"templates/summary.html",
                 controller:"summary"
             }
         }
-
     })
 
         .state("page_two",{
