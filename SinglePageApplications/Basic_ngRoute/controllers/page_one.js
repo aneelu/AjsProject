9ecc43488014424415eb52b51d87bdc1ce68cb76
Samba <samba.pendela@gmail.com>
2017-04-09 11:50:56 +0530

    Single page Applications

---
+++ SinglePageApplications/Basic_ngRoute/controllers/page_one.js
@@ -0,0 +1,5 @@
+app.controller("page_one",page_one);
+page_one.$inject=["$scope"];
+function page_one($scope) {
+    $scope.var_one="Data From MySQL DB Soon...";
+}
---
+++ SinglePageApplications/NamedViews_uiRouter/config.js
@@ -0,0 +1,33 @@
+app.config(config);
+config.$inject=["$stateProvider","$urlRouterProvider"];
+function config($stateProvider,$urlRouterProvider) {
+    $stateProvider.state("page_one",{
+       url:"/page_one",
+        views:{
+            '':{
+                templateUrl:"templates/page_one.html",
+                controller:"page_one"
+            },
+
+            'calc@page_one':{
+                templateUrl:"templates/calc.html",
+                controller:"calc"
+            },
+
+            'summary@page_one':{
+                templateUrl:"templates/summary.html",
+                controller:"summary"
+            }
+        }
+
+    })
+
+        .state("page_two",{
+            url:"/page_two",
+            templateUrl:"templates/page_two.html",
+            controller:"page_two"
+        });
+
+
+    $urlRouterProvider.otherwise("/page_one");
+}
