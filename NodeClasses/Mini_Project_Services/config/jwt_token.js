ed5e0d0e5547b0fe2925a8410c4a7755b689f10e
Samba <samba.pendela@gmail.com>
2017-04-19 08:40:34 +0530

    Mini_Project Files

---
+++ NodeClasses/Mini_Project_Services/config/jwt_token.js
@@ -0,0 +1,6 @@
+module.exports = {
+    user:"",
+    password:"",
+    token:""
+};
+
--- NodeClasses/Mini_Project_Services/server.js
+++ NodeClasses/Mini_Project_Services/server.js
@@ -5,6 +5,7 @@ var app = express();
 app.use(express.static(__dirname+"/../Mini_Project_Service"));
 app.use(bodyparser.json());
 var config = require("./config/config");
+var jwt_token = require("./config/jwt_token");
 
 var connection=mysql.createConnection({
     host:config.host,
@@ -16,11 +17,24 @@ var connection=mysql.createConnection({
 
 connection.connect();
 
-app.get("/login",function (req,res) {var host = config.host;
+app.get("/login",function (req,res) {
+
+    jwt_token.user="Hello";
+    jwt_token.password="Hello";
+
+
     connection.query("select * from user_details",function (err,records,fields) {
-       res.send(records);
+        res.send({"token":jwt_token.token});
     });
 });
+
+
+
+
+var token = require("./routes/token");
+app.use("/token",token);
+
+
 
 app.listen(8080);
 console.log("---Server Listening the Port No.8080---");
