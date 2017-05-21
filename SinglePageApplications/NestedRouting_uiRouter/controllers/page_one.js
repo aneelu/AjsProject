7437d3a1489be41a89f53a3df99225b636d0b10c
NareshIT <hr@nareshit.in>
2017-05-21 17:26:21 +0530

    SPA_2

---
+++ SinglePageApplications/NestedRouting_uiRouter/controllers/page_one.js
@@ -0,0 +1,5 @@
+app.controller("page_one",page_one);
+page_one.$inject=["$scope"];
+function page_one($scope){
+	$scope.var_one = "I am from page one controller";
+}
---
+++ SinglePageApplications/NestedRouting_uiRouter/controllers/page_two.js
@@ -0,0 +1,5 @@
+app.controller("page_two",page_two);
+page_two.$inject=["$scope"];
+function page_two($scope){
+	$scope.var_two = "I am from page two controller";
+}
