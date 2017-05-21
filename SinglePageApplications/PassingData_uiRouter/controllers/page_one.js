5824a9aae22c218adb6eb02bf1febdd5faaa85fe
NareshIT <hr@nareshit.in>
2017-05-21 16:52:21 +0530

    SPA_1

---
+++ SinglePageApplications/PassingData_uiRouter/controllers/page_one.js
@@ -0,0 +1,5 @@
+﻿app.controller("page_one", page_one);
+page_one.$inject = ["$scope","$stateParams"];
+function page_one($scope, $stateParams) {
+    $scope.var_one = $stateParams.id + "..." + $stateParams.name + "..." + $stateParams.age;
+}
