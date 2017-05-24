fcedba9e3609d24b4f1aae9f95a2149af979b4bd
NareshIT <hr@nareshit.in>
2017-05-24 20:32:39 +0530

    Directory Structure

--- MiniProject/controllers/loginController.js
+++ MiniProject/controllers/loginController.js
@@ -1,9 +1,11 @@
 app.controller("loginController",loginController);
-loginController.$inject=["$scope","loginService","$location"];
-function loginController($scope,loginService,$location) {
+loginController.$inject=["$scope","loginService","$location","$localStorage"];
+function loginController($scope,loginService,$location,$localStorage) {
     $scope.login_details = function (data) {
         loginService.authenicate(data).then(function (res) {
+
             if(res.data.login == 'success'){
+                $localStorage.poc = res;
                 $location.path("/home");
             }else{
                 alert("Invalid Details !");
--- MiniProject/server.js
+++ MiniProject/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
+var mysql = require("mysql");
+var jwt = require("jwt-simple");
 app.use(bodyparser.json());
 app.use(express.static(__dirname+"/../MiniProject"));
 
@@ -8,17 +10,32 @@ app.get("/",function (req,res) {
    res.redirect("/index.html");
 });
 
+var connection = mysql.createConnection({
+   host:"localhost",
+    user:"root",
+    password:"root",
+    database:"poc"
+});
+
+connection.connect();
+
+
+var tokens=[];
 
 app.post("/login",function (req,res) {
     var uname = req.body.uname;
     var upwd = req.body.upwd;
 
-    if(uname == 'admin' && upwd=='admin'){
-        res.send({'login':'success'});
-    }else{
-        res.send({'login':'failure'});
-    }
-
+    connection.query("select * from login_details where uname='"+uname+"'",
+                    function (err,records,fields) {
+       if(records.length>0){
+           var token = jwt.encode({'uname':uname},'hr@tcs.com');
+           tokens.push(token);
+           res.send({'login':'success','token':token});
+       } else{
+           res.send({'login':'failure'});
+       }
+    });
 
 
 });
