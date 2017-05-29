b84a2a665a753d112db5c379d100c5055ec7fb22
NareshIT <hr@nareshit.in>
2017-05-29 20:32:29 +0530

    Mini Project Completed !

--- Basic_Angular2App/app/app.component.ts
+++ Basic_Angular2App/app/app.component.ts
@@ -1,10 +1,23 @@
 import {Component} from 'angular2/core';
+import {MyService} from './app.service';
 
 @Component({
     selector: 'my-app',
-    template: "<input [(ngModel)]='var_two'/> {{var_one}}"
+    template: "<input [(ngModel)]='var_one'/>" +
+    "{{var_one}}",
+    providers : [MyService]
 
 })
 export class AppComponent {
-    var_two : string = "Welcome to Angular2"
+    var_one : string="";
+    constructor(obj : MyService){
+
+        this.var_one = "Welcome To Angular2";
+    }
+
+
+    my_fun(){
+        alert("Welcome to Angular2");
+    }
+
 }
---
+++ Basic_Angular2App/app/app.service.ts
@@ -0,0 +1,5 @@
+export class MyService{
+    getData(){
+        return "Data From MongoDB Soon..";
+    }
+}
