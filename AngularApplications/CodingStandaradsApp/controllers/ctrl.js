c46a56926fadfd0236d9715151396224e23f2cb6
Samba <samba.pendela@gmail.com>
2017-06-03 16:35:37 +0530

    ngAppDemo

---
+++ AngularApplications/CodingStandaradsApp/controllers/ctrl.js
@@ -0,0 +1,8 @@
+app.controller("ctrl",ctrl);
+ctrl.$inject=["$scope"];
+function ctrl($scope) {
+
+    $scope.var_one = "AngularJS";
+    $scope.var_two = "NodeJS";
+    $scope.var_three = "MongoDB";
+}
