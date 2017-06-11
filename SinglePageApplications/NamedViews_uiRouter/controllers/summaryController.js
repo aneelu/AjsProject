b28c4d267c7035e26977a7180b39d6e1242722b6
Samba <samba.pendela@gmail.com>
2017-06-11 17:52:25 +0530

    SPA Examples !!!

---
+++ SinglePageApplications/NamedViews_uiRouter/controllers/summaryController.js
@@ -0,0 +1,5 @@
+app.controller("summaryController",summaryController);
+summaryController.$inject=["$scope"];
+function summaryController($scope) {
+    $scope.summary="Consolidate Report Soon....";
+}
