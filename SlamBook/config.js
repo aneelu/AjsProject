95557bf4334b562016187db9f2e8085a2b6b47e8
NareshIT <hr@nareshit.in>
2017-06-19 19:39:37 +0530

    Custom Services Communication !!!

--- SlamBook/config.js
+++ SlamBook/config.js
@@ -1,5 +1,13 @@
 (function () {
     "use strict";
+
+    app.config(function(socialProvider){
+        socialProvider.setGoogleKey("1071246704138-s3jduierebq267jd4eq0spohbgcuk3o7.apps.googleusercontent.com");
+        socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
+        socialProvider.setFbKey({appId: "YOUR FACEBOOK APP ID", apiVersion: "API VERSION"});
+    });
+
+
     app.config(config);
     config.$inject=["$stateProvider","$urlRouterProvider"];
     function config($stateProvider,$urlRouterProvider) {
