c3e9cda672dded29013675a572cddd49ae8b92b3
Samba <tejaswi.af257@gmail.com>
2017-06-27 09:22:57 +0530

    MongoDB Data

--- NodeJS/Read_MongoDB/server.js
+++ NodeJS/Read_MongoDB/server.js
@@ -2,22 +2,18 @@ var express = require("express");
 var mongodb = require("mongodb");
 var app = express();
 
-app.use(express.static(__dirname+"/../ReadMongoDB"));
+app.use(express.static(__dirname+"/../Read_MongoDB"));
 
-//defualt output.
 app.get("/",function (req,res) {
-    res.redirect("/index.html");
+   res.redirect("/index.html");
 });
-
-var durgasoft = mongodb.MongoClient;
-
-app.get("/mongo",function (req,res) {
-    durgasoft.connect("mongodb://localhost:27017/mydb",function (err,db) {
+var mongoClient = mongodb.MongoClient;
+app.get("/emp",function (req,res) {
+    mongoClient.connect("mongodb://localhost:27017/test",function (err,db) {
         db.collection("emp").find().toArray(function (err, array) {
             res.send(array);
         });
     });
 });
-
 app.listen(8080);
 console.log("Server Listening the Port No.8080");
