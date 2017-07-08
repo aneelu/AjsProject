9a5d3492101300615171a8959cd8454527c71bef
NareshIT <hr@nareshit.in>
2017-07-08 12:53:01 +0530

    Single Page Applications

---
+++ AngularApplications/DynamicAngularApplication/controllers/ctrl.js
@@ -0,0 +1,7 @@
+app.controller("ctrl",ctrl);  //arg1 -  name of the controller
+                              //arg2 - name of the user defined function
+function ctrl($scope) {
+    $scope.sub_one = "AngularJS";
+    $scope.sub_two = "NodeJS";
+    $scope.sub_three = "MongoDB";
+};
---
+++ AngularApplications/TestExample/controllers/test.js
@@ -0,0 +1,6 @@
+app.controller("test" , test);
+
+function test($scope) {
+    $scope.num_one = 100;
+    $scope.num_two = 200;
+}
